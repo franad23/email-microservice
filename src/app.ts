84d321ce71501809f81464e08415e53301205b0d
Franco Diaz <dfrancoandres@gmail.com>
2024-08-30 21:07:56 -0300

    delete network, and add new message in / route

--- src/app.ts
+++ src/app.ts
@@ -6,11 +6,14 @@ import emailServiceRoutes from './routes/emailService.routes';
 const app = express();
 
 app.use(cors({
-  origin: '*',
+    origin: '*',
 }));
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', emailServiceRoutes);
+app.get('/', (req, res) => {
+  res.send('Email Microservice API is running');
+});
 
 export default app;
